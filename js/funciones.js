c10960ae9b103b5d01648e6e1289d16664618291
thidario <69899381+thidario@users.noreply.github.com>
2020-09-28 00:20:42 +0200

    Boi Neon
    Proyecto diseño web - película Boi Neon

---
+++ js/funciones.js
@@ -0,0 +1,102 @@
+(function($) {
+
+  'use strict';
+
+  var loader = function() {
+    setTimeout(function() { 
+      if($('#loader').length > 0) {
+        $('#loader').removeClass('show');
+      }
+    }, 1);
+  };
+  loader();
+
+  
+  $('nav .dropdown').hover(function(){
+    var $this = $(this);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    $this.find('.dropdown-menu').addClass('show');
+  }, function(){
+    var $this = $(this);
+      $this.removeClass('show');
+      $this.find('> a').attr('aria-expanded', false);
+      $this.find('.dropdown-menu').removeClass('show');
+  });
+
+  $('.navbar .dropdown > a').click(function(){
+    location.href = this.href;
+  });
+
+  $('#dropdown04').on('show.bs.dropdown', function () {
+    console.log('show');
+  });
+  
+  
+  var siteStellar = function () {
+    $(window).stellar({
+      responsive: false,
+      parallaxBackgrounds: true,
+      parallaxElements: true,
+      horizontalScrolling: false,
+      hideDistantElements: false,
+      scrollProperty: 'scroll'
+    });
+  };
+  siteStellar();
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.element-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .element-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn element-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft element-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight element-animated');
+							} else {
+								el.addClass('fadeInUp element-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 100);
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+  var slideImage = function() {
+    $(".slide-image").each(function () {
+      var bg = $(this).data("bg");
+      var pos = $(this).data("kenburn-start");
+
+      $(this).css({
+        "background-image": "url(" + bg + ")",
+        "transform-origin": pos
+      });
+    });
+  };
+  slideImage();
+  
+  
+
+})(jQuery);
+
+
+
